be17476f0e223ee11995f02edb072aa819f7043b
saragarcia6123 <saragarcia6123@gmail.com>
2025-06-03 13:38:02 +0200

    load dotenv

---
+++ src/lib/config.ts
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const config = {
+  DB_HOST: process.env.DB_HOST || '',
+  DB_USER: process.env.DB_USER || '',
+  DB_PASSWORD: process.env.DB_PASSWORD || '',
+  DB_NAME: process.env.DB_NAME || '',
+};
